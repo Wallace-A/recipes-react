4d03d71418e966f59a218312fcfe708aa2a4a91e
Andrew Wallace <andrew.wallace.english@gmail.com>
2021-03-31 19:33:30 +0900

    connected and recieving from API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
 import './App.css';
 
-function App() {
-  return (
+const App = () => {
+  //app id
+  const APP_ID = "0b2ff9d2";
+  const API_KEY = "1d2182efb99e8f3de04a946c5bbd41b7";
+
+  useEffect( () => {
+    getRecipes();
+  },[]);
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${API_KEY}`);
+    const data = response.json();
+    console.log(data);
+  }
+  
+  return(
     <div className="App">
       <h1>sup</h1>
+      <form className="search-form">
+        <input className="search-bar"type="text"/>
+        <button className="search-input" type="submit"></button>
+      </form>
     </div>
   );
 }
+
+
 
 export default App;
